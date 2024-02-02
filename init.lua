649e83ac2105782d75afc8ebdfbcee9707fd7527
Juan <juan.carrasco@syngenta.com>
2024-02-02 22:20:30 +0100

    first commit

---
+++ init.lua
@@ -0,0 +1,21 @@
+vim.g.mapleader = " "
+vim.api.nvim_set_keymap('i', 'jk', '<Esc>', { noremap = true })
+vim.api.nvim_set_keymap('v', 'jk', '<Esc>', { noremap = true })
+vim.o.number = true
+vim.o.relativenumber = true
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+if not vim.loop.fs_stat(lazypath) then
+  vim.fn.system({
+    "git",
+    "clone",
+    "--filter=blob:none",
+    "https://github.com/folke/lazy.nvim.git",
+    "--branch=stable", -- latest stable release
+    lazypath,
+  })
+end
+vim.opt.rtp:prepend(lazypath)
+require("lazy").setup("plugins")
+vim.o.background = "dark" -- or "light" for light mode
+vim.cmd([[colorscheme gruvbox]])
+vim.opt.mouse = ""
---
+++ lua/plugins/themes.lua
@@ -0,0 +1,5 @@
+return {
+	"ellisonleao/gruvbox.nvim",
+	tag = "2.0.0",
+	priority = 1000
+}
