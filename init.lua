4d3c0412c16ac4f44768eef21f222ed546767085
Juan <juan.carrasco@syngenta.com>
2024-02-17 13:20:25 +0100

    Moved vim optiones to its own file

--- init.lua
+++ init.lua
@@ -1,15 +1,3 @@
-vim.o.expandtab=true
-vim.o.tabstop=2
-vim.o.softtabspot=2
-vim.o.shiftwidth=2
-
-vim.g.loaded_netrw       = 1
-vim.g.loaded_netrwPlugin = 1
-vim.g.mapleader = " "
-vim.api.nvim_set_keymap('i', 'jk', '<Esc>', { noremap = true })
-vim.api.nvim_set_keymap('v', 'jk', '<Esc>', { noremap = true })
-vim.o.number = true
-vim.o.relativenumber = true
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not vim.loop.fs_stat(lazypath) then
   vim.fn.system({
@@ -23,6 +11,5 @@ if not vim.loop.fs_stat(lazypath) then
 end
 vim.opt.rtp:prepend(lazypath)
 require("lazy").setup("plugins")
-vim.o.background = "dark" -- or "light" for light mode
-vim.cmd([[colorscheme gruvbox]])
-vim.opt.mouse = ""
+require("vim-options")
+
---
+++ lua/vim-options.lua
@@ -0,0 +1,16 @@
+vim.o.expandtab=true
+vim.o.tabstop=2
+vim.o.softtabspot=2
+vim.o.shiftwidth=2
+
+vim.g.loaded_netrw       = 1
+vim.g.loaded_netrwPlugin = 1
+vim.g.mapleader = " "
+vim.api.nvim_set_keymap('i', 'jk', '<Esc>', { noremap = true })
+vim.api.nvim_set_keymap('v', 'jk', '<Esc>', { noremap = true })
+vim.o.number = true
+vim.o.relativenumber = true
+
+vim.o.background = "dark" -- or "light" for light mode
+vim.cmd([[colorscheme gruvbox]])
+vim.opt.mouse = ""
