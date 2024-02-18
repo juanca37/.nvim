f8c9bc5ec2fb1769fbbed089a485c78e158b284c
Juan <juan.carrasco@syngenta.com>
2024-02-18 20:00:06 +0100

    Adding cmp and snippets config

--- lua/plugins/lsp-config.lua
+++ lua/plugins/lsp-config.lua
@@ -2,29 +2,32 @@ return {
   {
     "williamboman/mason.nvim",
     config = function()
-      require("mason").setup({
-
-      })
-    end
+      require("mason").setup({})
+    end,
   },
   {
     "williamboman/mason-lspconfig.nvim",
     config = function()
       require("mason-lspconfig").setup({
-        ensure_installed = { "lua_ls", "pyright" }
+        ensure_installed = { "lua_ls", "pyright" },
       })
-    end
+    end,
   },
   {
     "neovim/nvim-lspconfig",
-    config = function ()
+    config = function()
+      local capabilities = require("cmp_nvim_lsp").default_capabilities()
       local lspconfig = require("lspconfig")
-      lspconfig.lua_ls.setup({})
-      lspconfig.pyright.setup({})
-      vim.keymap.set('n', 'K', vim.lsp.buf.hover, {})
-      vim.keymap.set('n', 'gD', vim.lsp.buf.declaration, {})
-      vim.keymap.set('n', 'gd', vim.lsp.buf.definition, {})
-      vim.keymap.set({ 'n', 'v' }, '<space>ca', vim.lsp.buf.code_action, { desc = "LSP code actions" })
-    end
-  }
+      lspconfig.lua_ls.setup({
+        capabilities = capabilities,
+      })
+      lspconfig.pyright.setup({
+        capabilities = capabilities,
+      })
+      vim.keymap.set("n", "K", vim.lsp.buf.hover, {})
+      vim.keymap.set("n", "gD", vim.lsp.buf.declaration, {})
+      vim.keymap.set("n", "gd", vim.lsp.buf.definition, {})
+      vim.keymap.set({ "n", "v" }, "<space>ca", vim.lsp.buf.code_action, { desc = "LSP code actions" })
+    end,
+  },
 }
