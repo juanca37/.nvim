a9a90e997275c02a76d5581d8aa10969e36a5d25
Juan <juan.carrasco@syngenta.com>
2024-02-22 11:57:52 +0100

    Adding completions

--- lua/vim-options.lua
+++ lua/vim-options.lua
@@ -1,12 +1,12 @@
-vim.o.expandtab          = true
-vim.o.tabstop            = 2
-vim.o.softtabspot        = 2
-vim.o.shiftwidth         = 2
+vim.o.expandtab = true
+vim.o.tabstop = 2
+vim.o.softtabspot = 2
+vim.o.shiftwidth = 2
 
-vim.g.loaded_netrw       = 1
+vim.g.loaded_netrw = 1
 vim.g.loaded_netrwPlugin = 1
-vim.g.mapleader          = " "
-vim.api.nvim_set_keymap('i', 'jk', '<Esc>', { noremap = true })
+vim.g.mapleader = " "
+vim.api.nvim_set_keymap("i", "jk", "<Esc>", { noremap = true })
 vim.o.number = true
 vim.o.relativenumber = true
 
@@ -14,3 +14,5 @@ vim.o.background = "dark" -- or "light" for light mode
 vim.cmd([[let g:gruvbox_material_background = 'soft']])
 vim.cmd([[colorscheme gruvbox-material]])
 vim.opt.mouse = ""
+
+vim.o.splitright = true
