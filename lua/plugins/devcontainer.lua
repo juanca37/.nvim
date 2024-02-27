2b98298ca35a1dd9350b3e7cd9f0b763f0165172
Juan <juan.carrasco@syngenta.com>
2024-02-27 12:18:20 +0100

    Adding devcontainer opt

--- lua/plugins/devcontainer.lua
+++ lua/plugins/devcontainer.lua
@@ -1,7 +1,13 @@
 return {
-  'https://codeberg.org/esensar/nvim-dev-container',
-  dependencies = 'nvim-treesitter/nvim-treesitter',
-  config = function()
-	require('devcontainer').setup({})
-  end
+	"https://codeberg.org/esensar/nvim-dev-container",
+	dependencies = "nvim-treesitter/nvim-treesitter",
+	config = function()
+		require("devcontainer").setup({
+			attach_mounts = {
+				neovim_config = {
+					enabled = true,
+				},
+			},
+		})
+	end,
 }
