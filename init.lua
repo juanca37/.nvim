7aa511674faf899b398e49dd9abcddcbf55ea595
Juan <juan.carrasco@syngenta.com>
2024-03-08 23:21:21 +0100

    Added lsp lines plugin

--- init.lua
+++ init.lua
@@ -1,3 +1,5 @@
+vim.opt.termguicolors = true
+
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not vim.loop.fs_stat(lazypath) then
   vim.fn.system({
@@ -12,4 +14,3 @@ end
 vim.opt.rtp:prepend(lazypath)
 require("lazy").setup("plugins")
 require("vim-options")
-
---
+++ lua/plugins/lsp_lines.lua
@@ -0,0 +1,7 @@
+return {
+	"Maan2003/lsp_lines.nvim",
+  config = function ()
+    require("lsp_lines").setup()
+    
+  end
+}
