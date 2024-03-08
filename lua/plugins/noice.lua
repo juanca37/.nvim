6165d1eea2dc7de6e6f80d4971b5f20327b8f1d5
Juan <juan.carrasco@syngenta.com>
2024-03-08 23:54:48 +0100

    Adding Incremental rename

--- lua/plugins/noice.lua
+++ lua/plugins/noice.lua
@@ -11,5 +11,10 @@ return {
     --   `nvim-notify` is only needed, if you want to use the notification view.
     --   If not available, we use `mini` as the fallback
     "rcarriga/nvim-notify",
-    }
+  },
+  config = function()
+    require("noice").setup({
+      presets = { inc_rename = true }
+    })
+  end,
 }
