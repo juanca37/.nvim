d427766c591233f19864974996aa664949ef0747
juanca37 <juancarrascoalonso@hotmail.com>
2024-04-02 08:56:55 +0200

    feat: adding all before deletion of folder

---
+++ lua/plugins/debugger.lua
@@ -0,0 +1,37 @@
+return {
+  {
+    "mfussenegger/nvim-dap",
+    dependencies = {
+      "rcarriga/nvim-dap-ui",
+    },
+    config = function()
+      local dap, dapui = require("dap"), require("dapui")
+      dap.listeners.before.attach.dapui_config = function()
+        dapui.open()
+      end
+      dap.listeners.before.launch.dapui_config = function()
+        dapui.open()
+      end
+      dap.listeners.before.event_terminated.dapui_config = function()
+        dapui.close()
+      end
+      dap.listeners.before.event_exited.dapui_config = function()
+        dapui.close()
+      end
+      vim.keymap.set("n", "<leader>dc", function()
+        dap.continue()
+      end)
+      vim.keymap.set("n", "<leader>ds", function()
+        dap.step_over()
+      end)
+      vim.keymap.set("n", "<Leader>dt", function()
+        dap.toggle_breakpoint()
+      end)
+      vim.keymap.set("n", "<Leader>dB", function()
+        dap.set_breakpoint()
+      end)
+    end,
+  },
+  {
+  },
+}
